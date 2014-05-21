c80640541ece6288db30bc63555a4e61f9b3873d
W-Sarah <sarahweiss75@gmail.com>
2014-05-21 18:13:16 +0900

    adding option to go directly to game if user already cookied

--- tl-blackjack-webapp/main.rb
+++ tl-blackjack-webapp/main.rb
@@ -4,10 +4,18 @@ require 'sinatra'
 set :sessions, true
 
 get '/' do
+  if session[:player_name]
+    redirect '/game'
+  else
+  redirect '/new_player'
+  end 
+end
+
+get '/new_player' do
   erb :username_form
 end
 
-post '/username_filled' do
+post '/new_player' do
   session[:player_name] = params[:player_name]
   redirect '/game'
 end
